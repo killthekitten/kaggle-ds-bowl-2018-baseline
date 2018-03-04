76a194b01e595836db063a4f673fcdc772523ed1
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2018-03-04 15:33:07 +0300

    Remove the occlusion handling code from dataset

--- bowl_dataset.py
+++ bowl_dataset.py
@@ -6,39 +6,39 @@ import re
 import cv2
 
 class BowlDataset(Dataset):
-    
-    
+
+
     def load_bowl(self, base_path):
         """Generate the requested number of synthetic images.
         count: number of images to generate.
         height, width: the size of the generated images.
         """
         self.add_class("bowl", 1, "nuclei")
-        
+
         masks = dict()
         id_extractor = re.compile(base_path + "\/(?P<image_id>.*)\/masks\/(?P<mask_id>.*)\.png")
-        
+
         for mask_path in glob(os.path.join(base_path, "**", "masks", "*.png")):
             matches = id_extractor.match(mask_path)
-            
+
             image_id = matches.group("image_id")
             image_path = os.path.join(base_path, image_id, "images", image_id + ".png")
-            
+
             if image_path in masks:
-                masks[image_path].append(mask_path)            
+                masks[image_path].append(mask_path)
             else:
                 masks[image_path] = [mask_path]
-        
-        for i, (image_path, mask_paths) in enumerate(masks.items()): 
+
+        for i, (image_path, mask_paths) in enumerate(masks.items()):
             self.add_image("bowl", image_id=i, path=image_path, mask_paths=mask_paths)
 
-            
+
     def load_image(self, image_id):
         info = self.image_info[image_id]
-        
+
         return cv2.imread(info["path"])
 
-    
+
     def image_reference(self, image_id):
         """Return the shapes data of the image."""
         info = self.image_info[image_id]
@@ -47,24 +47,18 @@ class BowlDataset(Dataset):
         else:
             super(self.__class__).image_reference(self, image_id)
 
-            
+
     def load_mask(self, image_id):
         info = self.image_info[image_id]
         mask_paths = info["mask_paths"]
         count = len(mask_paths)
         masks = []
-        
+
         for i, mask_path in enumerate(mask_paths):
             masks.append(cv2.imread(mask_path, 0))
-            
+
         masks = np.stack(masks, axis=-1)
         masks = np.where(masks > 128, 1, 0)
-        
-#         Handle occlusions
-        occlusion = np.logical_not(masks[:, :, -1]).astype(np.uint8)
-        for i in range(count-2, -1, -1):
-            masks[:, :, i] = masks[:, :, i] * occlusion
-            occlusion = np.logical_and(occlusion, np.logical_not(masks[:, :, i]))
         
         class_ids = np.ones(count)
         return masks, class_ids.astype(np.int32)
